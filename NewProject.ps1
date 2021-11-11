af1a925c9c536218b2ce6ad5d623027d0ab2d598
simonen <si.petrov@yahoo.com>
2021-11-11 08:58:36 +0200

    Add files via upload

---
+++ NewProject.ps1
@@ -0,0 +1,44 @@
+﻿Write-Host "####################################"
+Write-Host "### CREATE NEW PROJECT DIRECTORY ###"
+Write-Host "####################################"
+
+$projNumber = Read-Host "Enter Project Number " -Verbose
+$projLocation = Read-Host "Enter Project Location " -Verbose
+$projClient = Read-Host "Enter Client Name " -Verbose
+
+$projDir = $projNumber+"_"+$projLocation+"_"+$projClient
+
+New-Item -Type Directory -Path E:\ -Name $projDir -Verbose
+Echo "Folder Created"
+
+#New-Item -Type Directory -Path E:\$projDir -Name RENDER 
+1..4 | ForEach-Object {New-Item -Type Directory -Path E:\$projDir -Name RENDER\CGI0$_}
+New-Item -Type Directory -Path E:\$projDir -Name SCENES\MAPS
+New-Item -Type Directory -Path E:\$projDir -Name SOURCE
+
+#Create the project folder on the server
+
+New-Item -ItemType Directory -Path '\\192.168.17.239\Visualization\02 - Simeon\' -Name $projDir -Verbose
+
+Write-Host 
+Write-Host "***/TEMP QUERY***"
+Write-Host 
+
+$tempFolder = "\\192.168.17.239\Temp\Simo"
+Get-ChildItem -path $tempFolder
+Write-Host 
+Write-Host 
+$selFolder = Read-Host("***SELECT FOLDER***") 
+$finalFolder = Get-Item -Path $tempFolder\*$selFolder*
+$finalFolder
+$finalFolder.count
+
+#Pull files from TEMP to PROJECT\SOURCE folder
+
+Test-Path $finalFolder
+
+pause
+
+Copy-Item -Path $finalFolder\* -Destination e:\$projDir\SOURCE -Recurse -Verbose
+
+pause
---
+++ copyJGPs.ps1
@@ -0,0 +1,19 @@
+﻿Write-Host "####################################################"
+Write-Host "####COPY THE JPGs FROM LOCAL TO SERVER DIRECTORY####" -ForegroundColor Cyan
+Write-Host "####################################################"
+Write-Host
+$number = Read-Host "Enter project number"
+$serverDir = '\\192.168.17.239\Visualization\02 - Simeon'
+$projectName = $(Get-ChildItem -Path $serverDir\$number*)
+Write-Host ""
+Write-Host "COPYING FILES FROM: $(get-childitem -Path e:\$number*) TO: $projectName\"
+Write-Host ""
+Copy-Item -Path e:\$number*\*.jpg -Destination $projectName -Verbose
+Write-Host 
+Write-Host '***'$(get-childitem -Path e:\$number*\*.jpg).count 'ITEMS COPIED ***'
+Write-Host 
+Write-Host "**** $projectName **** COPIED TO CLIPBOARD ****" 
+Write-Host 
+Set-Clipboard -Value "$projectName"
+pause
+
